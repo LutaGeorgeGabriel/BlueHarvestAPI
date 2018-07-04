d6c31147e635faf922649af55204fe64b1f076bc
Luta George Gabriel <luta.george.gabriel@gmail.com>
2018-07-04 21:24:04 +0300

    created User model

---
+++ src/main/java/application/model/User.java
@@ -0,0 +1,77 @@
+package application.model;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+// TODO: maybe use Lombok?
+
+public class User {
+
+    private UUID customerID;
+    private String name;
+    private String surname;
+    private List<Account> accounts = new ArrayList<>();
+
+    // needed for serialization
+    public User() {
+    }
+
+    public User(UUID customerID, String name, String surname, List<Account> accounts) {
+        this.customerID = customerID;
+        this.name = name;
+        this.surname = surname;
+        this.accounts = accounts;
+    }
+
+    public User(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public UUID getCustomerID() {
+        return customerID;
+    }
+
+    public void setCustomerID(UUID customerID) {
+        this.customerID = customerID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public List<Account> getAccounts() {
+        return accounts;
+    }
+
+    public void setAccounts(List<Account> accounts) {
+        this.accounts = accounts;
+    }
+
+    public void addAccount(Account account) {
+        this.accounts.add(account);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "customerID=" + customerID +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", accounts=" + accounts +
+                '}';
+    }
+}
