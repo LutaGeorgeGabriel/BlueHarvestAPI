d451ad459fef179d8ec56a72b7c0d01460d32777
Luta George Gabriel <luta.george.gabriel@gmail.com>
2018-07-05 13:11:13 +0300

    added BlueHarvestApi as the root component

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,26 +1,11 @@
 import React from 'react';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import FlatButton from 'material-ui/FlatButton';
-import Service from './service/Service'
-import User from './User'
-import UserCreateForm from './UserCreateForm'
+import BlueHarvestApi from './BlueHarvestAPI'
 
 import '../resources/App.css';
 
 const App = () => (
     <div className="App">
-        <MuiThemeProvider>
-            <UserCreateForm />
-            <FlatButton
-                label="GET"
-                secondary={true}
-                onClick={() => Service.fetch()}/>
-            <FlatButton
-                label="UPDATE"
-                secondary={true}
-                onClick={() => Service.update()}/>
-            <User />
-        </MuiThemeProvider>
+        <BlueHarvestApi/>
     </div>
 );
 
