40606ac3e8b2a9790839737d83c8ef8f35be6150
Luta George Gabriel <luta.george.gabriel@gmail.com>
2018-07-05 12:57:41 +0300

    created mock user and account to simulate a transaction

--- src/main/java/application/service/AccountService.java
+++ src/main/java/application/service/AccountService.java
@@ -5,15 +5,29 @@ import application.model.Transaction;
 import application.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.ArrayList;
+
+import static application.utils.Utils.generateUUID;
+
 public class AccountService {
 
     @Autowired
     TransactionService transactionService;
 
     public Account create(User user, double initialCredit) {
-        Account account = new Account(user, 0.0);
+        Account account = new Account(user.getCustomerID(), generateUUID(), 0.0);
         if(initialCredit != 0) {
-            Transaction transaction = transactionService.create(account, account, initialCredit);
+
+            // create mockUser - from which we take money
+            User mockUser = new User(generateUUID(), "Rothschild", "Family", new ArrayList<>());
+
+            // create mock account - from which we simulate a withdrawal
+            Account mockAccount = new Account(mockUser.getCustomerID(), generateUUID(), 100.0);
+
+            Transaction transaction = transactionService.create(mockAccount, account, initialCredit);
+            mockAccount.withdraw(initialCredit);
+            account.deposit(initialCredit);
+
             account.addTransaction(transaction);
         }
         return account;
